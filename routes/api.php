88f4ba6895458db6891cb572c219a155ab7212c3
Muhammad Hassan Javed <hassanhafiz44@gmail.com>
2022-06-06 00:58:49 +0500

    Customers api completed

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get("/customers", function () {
+    $auth_string = request()->input("auth");
+    if ($auth_string === '$2y$10$Wqtzg7YOGlX7MgXMVnubZO7vwAm3Tye5bWbx52ObZwycfegv38xy.') {
+        $members = new Collection(\App\Models\membership::all());
+        return $members;
+    }
+    return response()->json(["error" => "unauthorized"], 401);
 });
