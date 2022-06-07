1b39fa353f19800a6f11bc41a192f6f3c70cc1d7
Muhammad Hassan Javed <hassanhafiz44@gmail.com>
2022-06-07 12:25:27 +0500

    Displaying logout button

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,20 +1,3 @@
-<!--
-
-=========================================================
-* Volt Free - Bootstrap 5 Dashboard
-=========================================================
-
-* Product Page: https://themesberg.com/product/admin-dashboard/volt-bootstrap-5-dashboard
-* Copyright 2021 Themesberg (https://www.themesberg.com)
-* License (https://themesberg.com/licensing)
-
-* Designed and coded by https://themesberg.com
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. Please contact us to request a removal.
-
--->
 <!DOCTYPE html>
 <html lang="en">
 
@@ -219,6 +202,17 @@
               </li>
             </ul>
           </div>
+        </li>
+        <li class="nav-item">
+          <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+            @csrf
+          </form>
+          <a href="{{route('home')}}" class="nav-link d-flex align-items-center" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+            <span class="sidebar-icon">
+              <img src="{{asset('volt-theme/html&css/assets/img/brand/light.svg')}}" height="20" width="20" alt="Volt Logo">
+            </span>
+            <span class="mt-1 ms-1 sidebar-text">{{ __('Logout') }}</span>
+          </a>
         </li>
       </ul>
     </div>
